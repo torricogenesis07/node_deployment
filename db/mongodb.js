21d62e99211885061a63491510baf6c0f943b815
torricogenesis07 <torricogenesis07@gmail.com>
2022-06-05 21:27:47 -0400

    segundo commit correccion variable entorno

--- db/mongodb.js
+++ db/mongodb.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose')
 
 mongoose.connection.on('open', ()=> console.log('la base de datos esta conectada') )
 
-async function conexionBD({host, port, db_name}){
-    const URI = `mongodb://${host}:${port}/${db_name}`
+async function conexionBD(/*{host, port, db_name}*/e){
+    //const URI = `mongodb://${host}:${port}/${db_name}`
+    const URI = process.env.MONGODB_URI;
     await mongoose.connect(URI, {useNewUrlParser:true, useUnifiedTopology: true,  useCreateIndex: true})
 }
 module.exports = conexionBD
